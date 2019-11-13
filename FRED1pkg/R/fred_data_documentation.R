ef99075564981fc3ee3a4e52d89e94d3ca5ffd6f
Shafer Powell <shaferpowell@gmail.com>
2019-11-13 18:14:59 -0500

    Added documentation file in manual folder for FRED package.

---
+++ FRED1pkg/R/fred_data_documentation.R
@@ -0,0 +1,5 @@
+#Demo package for FRED 1
+
+#Support documentation available for download with complete and up-to-date dataset at https://roots.ornl.gov
+
+#This is a truncated document for the purpose of developing tools to handle FRED data, and should not be used for formal analysis of FRED
