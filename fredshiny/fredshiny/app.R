6ba372c2fb102b477affb0f6415ea2486852fdc6
Shafer Powell <shaferpowell@gmail.com>
2019-11-14 00:53:28 -0500

    Created data table for FRED

--- fredshiny/fredshiny/app.R
+++ fredshiny/fredshiny/app.R
@@ -1,57 +1,57 @@
-#
-# FRED
-#
+#########
+# FRED Shiny app by Thomas Geissberger & Shafer Powell
+#########
 
 library(shiny)
-library(ggplot2)  # for the diamonds dataset
+library(FRED1pkg) # package containing the FRED dataset
+library(ggplot2)
 
 ui <- fluidPage(
-  title = "Examples of DataTables",
-  sidebarLayout(
-    sidebarPanel(
-      conditionalPanel(
-        'input.dataset === "diamonds"',
-        checkboxGroupInput("show_vars", "Columns in diamonds to show:",
-                           names(diamonds), selected = names(diamonds))
-      ),
-      conditionalPanel(
-        'input.dataset === "mtcars"',
-        helpText("Click the column header to sort a column.")
-      ),
-      conditionalPanel(
-        'input.dataset === "iris"',
-        helpText("Display 5 records by default.")
-      )
+  titlePanel("FRED 1 data explorer"),
+
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+    column(4,
+           selectInput("bgpart",
+                       "Belowground part",
+                       c("All",
+                         unique(as.character(fred1$Belowground.part))))
     ),
-    mainPanel(
-      tabsetPanel(
-        id = 'dataset',
-        tabPanel("diamonds", DT::dataTableOutput("mytable1")),
-        tabPanel("mtcars", DT::dataTableOutput("mytable2")),
-        tabPanel("iris", DT::dataTableOutput("mytable3"))
-      )
+    column(4,
+           selectInput("family",
+                       "Plant family:",
+                       c("All",
+                         unique(as.character(fred1$Plant.taxonomy_Family_TPL))))
+    ),
+    column(4,
+           selectInput("pot",
+                       "Growth environment:",
+                       c("All",
+                         unique(as.character(fred1$Notes_In.situ..pot..or.hydroponic))))
     )
-  )
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("table")
 )
 
 server <- function(input, output) {
-  
-  # choose columns to display
-  diamonds2 = diamonds[sample(nrow(diamonds), 1000), ]
-  output$mytable1 <- DT::renderDataTable({
-    DT::datatable(diamonds2[, input$show_vars, drop = FALSE])
-  })
-  
-  # sorted columns are colored now because CSS are attached to them
-  output$mytable2 <- DT::renderDataTable({
-    DT::datatable(mtcars, options = list(orderClasses = TRUE))
-  })
-  
-  # customize the length drop-down menu; display 5 rows per page by default
-  output$mytable3 <- DT::renderDataTable({
-    DT::datatable(iris, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
-  })
-  
+
+  # Filter data based on selections
+  output$table <- DT::renderDataTable(DT::datatable({
+    data <- fred1
+    if (input$bgpart != "All") {
+      data <- data[data$Belowground.part == input$bgpart,]
+    }
+    if (input$family != "All") {
+      data <- data[data$Plant.taxonomy_Family_TPL == input$family,]
+    }
+    if (input$pot != "All") {
+      data <- data[data$Notes_In.situ..pot..or.hydroponic == input$pot,]
+    }
+    data
+  }))
+
 }
+
 
 shinyApp(ui, server)
