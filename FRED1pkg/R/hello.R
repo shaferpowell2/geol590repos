a4fece9a80ef5e06283ce30395138b67e06df6c3
Drew Steen <andrew.decker.steen@gmail.com>
2019-12-16 14:04:13 -0500

    commented out all code, which was unnecessary. Namespace and documentation are not rebuilt

--- FRED1pkg/R/hello.R
+++ FRED1pkg/R/hello.R
@@ -2,12 +2,12 @@
 # FRED package
 #########
 
-library(devtools)
-fred1 <- read.csv("C:/Users/Shaflet/Documents/school/geol_590/final_proj/FRED_2.1_20180824_for_r.csv")
-
-use_data(fred1)
-
-Authors@R: as.person(c(
-  "Shafer Powell <apowel19@vols.utk.edu> [aut, cre]",
-  "Thomas Geissberger <tgeissbe@vols.utk.edu> [aut, cre]"
-))
+# library(devtools)
+# #fred1 <- read.csv("C:/Users/Shaflet/Documents/school/geol_590/final_proj/FRED_2.1_20180824_for_r.csv")
+# 
+# #use_data(fred1)
+# 
+# Authors@R: as.person(c(
+#   "Shafer Powell <apowel19@vols.utk.edu> [aut, cre]",
+#   "Thomas Geissberger <tgeissbe@vols.utk.edu> [aut, cre]"
+# ))
